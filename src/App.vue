5add68be8866a2bbc50fd2c3536d57177147d28d
zikri ma <zikrima123@gmail.com>
2024-04-01 19:21:13 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ namaLengkap }}</h1>
+    <p>{{ biodata }}</p>
+
+    <h2>Keterampilan & Kemampuan:</h2>
+    <ul>
+      <li v-for="keterampilan in keterampilanKemampuan" :key="keterampilan">{{ keterampilan }}</li>
+    </ul>
+
+    <h2>Pengalaman:</h2>
+    <ul>
+      <li v-for="pengalaman in pengalamanDimiliki" :key="pengalaman.title">
+        <h3>{{ pengalaman.title }}</h3>
+        <p>{{ pengalaman.deskripsi }}</p>
+      </li>
+    </ul>
+  <div>
+    <ButtonX></ButtonX>
+  </div>
+
+  </div>
+</template>
+<script>
+import ButtonX from './components/Button1.vue'
+</script>
+
+<script setup>
+const namaLengkap = 'Zikri Maarif';
+const biodata = 'Seorang adalah mahasiswa yang sedang belajar tentang pengembangan web. Saya lahir pada tahun 2003, dan saat ini saya berumur 20 tahun';
+const keterampilanKemampuan = ['HTML', 'CSS', 'JavaScript'];
+const pengalamanDimiliki = [
+  { title: 'Magang di Perusahaan X', deskripsi: 'Belajar dasar-dasar pengembangan web dan bekerja dalam proyek kecil.' },
+  { title: 'Proyek Kuliah: Website Sederhana', deskripsi: 'Membuat website sederhana sebagai tugas kuliah.' }
+];
+
+</script>
+
---
+++ src/components/Button1.vue
@@ -0,0 +1,17 @@
+<template>
+    <button>
+        Selengkapnya
+    </button>
+</template>
+
+<!-- css vue 3 -->
+
+<style scoped>
+.btn{
+    background-color: red;
+    color: white;
+    font-size: 28px;
+    
+}
+
+</style>
